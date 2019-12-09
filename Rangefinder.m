8446e8462e0e641e626eaa86b11bc4a9d9e5f2f9
Thomas <thomas.jagielski@yahoo.com>
2019-12-09 13:24:51 -0500

    Add commented code

--- Rangefinder.m
+++ Rangefinder.m
@@ -1,12 +1,17 @@
 function rangedfft = Rangefinder(transform, low, high)
-
+% Function used to zero high frequency signals
+% INPUTS: transform - the FFT vector
+%         low - lower index threshold
+%         high - higher index threshold
+% OUTPUT: rangedfft - FFT vector with zeroed values lower than "low" and
+%                     higher than "high"
 for l=1:max(size(transform))
-    if l <= low
-        transform(l) = 0;
+    if l <= low % Compare index to the lower value
+        transform(l) = 0; % If the index is below the "low" index, zero it
     end
-    if l >= high
-        transform(l) = 0;
+    if l >= high % Compare index to the higher value
+        transform(l) = 0; % If the index is larger the "high" index, zero it
     end
 end
-rangedfft = transform;
+rangedfft = transform; % Set the output equal to the filtered FFT
 end
